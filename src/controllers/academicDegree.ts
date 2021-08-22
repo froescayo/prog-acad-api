5a3d2de8537e7fe4269478eb75799c412d50b0b5
Cayo Froes <cayofroes@hotmail.com>
2021-08-22 18:53:04 -0300

    fix: getAcademicDegree to getAcademicDegrees

--- src/controllers/academicDegree.ts
+++ src/controllers/academicDegree.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export async function getAcademicDegree(req: Request, res: Response) {
+export async function getAcademicDegrees(req: Request, res: Response) {
   const dbAcademicDegree = await req.db.AcademicDegreeRepository.findAll();
 
   return res.status(200).send(dbAcademicDegree);
--- src/routes/academicDegree.ts
+++ src/routes/academicDegree.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getAcademicDegree } from "../controllers";
+import { getAcademicDegrees } from "../controllers";
 import { auth } from "../middlewares/auth";
 
 const academicDegreeRoutes = Router();
 
-academicDegreeRoutes.get("/academicDegree", auth, getAcademicDegree);
+academicDegreeRoutes.get("/academicDegree", auth, getAcademicDegrees);
 
 export default academicDegreeRoutes;

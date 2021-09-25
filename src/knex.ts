6144d87365aed6486fbaecfd6877945f70f48750
Cayo Froes <cayofroes@hotmail.com>
2021-09-25 18:27:14 -0300

    feat: test pgparser

--- src/knex.ts
+++ src/knex.ts
@@ -1,10 +1,14 @@
 import { randomBytes } from "crypto";
 import Knex from "knex";
 import { env } from "./helpers/env";
+import { parse } from "pg-connection-string";
 
 // databaseName definition
 export const databaseName =
   process.env.NODE_ENV && process.env.NODE_ENV === "test" ? `test_${randomBytes(8).toString("hex")}` : env.DB_DATABASE;
+
+const databaseURL = process.env.DATABASE_URL || "link";
+const dataBaseUrlParsed = parse(databaseURL);
 
 const connection =
   process.env.NODE_ENV !== "production"
@@ -16,7 +20,14 @@ const connection =
         port: parseInt(env.DB_PORT || "5432", 10),
         ssl: true,
       }
-    : process.env.DATABASE_URL;
+    : {
+        database: dataBaseUrlParsed.database!,
+        host: dataBaseUrlParsed.host!,
+        user: dataBaseUrlParsed.user!,
+        password: dataBaseUrlParsed.password!,
+        port: dataBaseUrlParsed.port!,
+        ssl: true,
+      };
 
 // knex options const
 const options: Knex.Config = {
